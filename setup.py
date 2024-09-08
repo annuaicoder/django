34a2fdb61fe7e1ca61b31ed71ae1ef19e2765692
adminmetricvisio <admin@metricvisio.org>
2024-09-07 17:56:05 -0700

    Fix setup progress lines

--- setup.py
+++ setup.py
@@ -1,55 +1,62 @@
 import os
-import site
 import sys
 from distutils.sysconfig import get_python_lib
-
+import site
 from setuptools import setup
 
-# Allow editable install into user site directory.
-# See https://github.com/pypa/pip/issues/7953.
-site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+def print_progress(message):
+    """Prints a progress message to the standard output."""
+    sys.stdout.write(f"{message}\n")
+    sys.stdout.flush()
 
-# Warn if we are installing over top of an existing installation. This can
-# cause issues where files that were deleted from a more recent Django are
-# still present in site-packages. See #18115.
+# Determine if we should allow editable install into the user site directory.
+print_progress("Checking for user site directory installation option...")
+site.ENABLE_USER_SITE = "--user" in sys.argv
+
+# Flag to indicate if we should display a warning about potential overlay issues
 overlay_warning = False
+
 if "install" in sys.argv:
+    print_progress("Installation detected. Checking for existing Django installations...")
+
+    # List of possible library paths to check for existing installations
     lib_paths = [get_python_lib()]
+
+    # Add /usr/local to the list of paths to handle custom Debian installations
     if lib_paths[0].startswith("/usr/lib/"):
-        # We have to try also with an explicit prefix of /usr/local in order to
-        # catch Debian's custom user site-packages directory.
         lib_paths.append(get_python_lib(prefix="/usr/local"))
+
+    # Check each library path for existing Django installations
     for lib_path in lib_paths:
         existing_path = os.path.abspath(os.path.join(lib_path, "django"))
         if os.path.exists(existing_path):
-            # We note the need for the warning here, but present it after the
-            # command is run, so it's more likely to be seen.
+            # Flag to display warning if Django is already installed
             overlay_warning = True
+            print_progress(f"Existing Django installation found at: {existing_path}")
             break
 
-
+# Set up the package using setuptools
+print_progress("Setting up the package with setuptools...")
 setup()
 
-
+# Display a warning if an existing Django installation was detected
 if overlay_warning:
     sys.stderr.write(
         """
-
 ========
 WARNING!
 ========
 
-You have just installed Django over top of an existing
-installation, without removing it first. Because of this,
-your install may now include extraneous files from a
-previous version that have since been removed from
-Django. This is known to cause a variety of problems. You
-should manually remove the
+You have just installed Django over an existing installation without removing it first.
+This may result in your install containing outdated or extraneous files from a previous version, which can lead to various issues.
+
+To resolve this, manually remove the following directory and then reinstall Django:
 
 %(existing_path)s
-
-directory and re-install Django.
 
 """
         % {"existing_path": existing_path}
     )
+    print_progress("Warning issued. Please follow the instructions above to avoid potential issues.")
+else:
+    print_progress("Installation completed successfully.")
